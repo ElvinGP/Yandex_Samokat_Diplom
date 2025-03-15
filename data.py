64e0184d0b13c0c868d0d16ae2204065178e9591
ElvinGP <bricks7@icloud.com>
2025-03-15 21:38:52 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # data.py
-order_data = {
+order_data_1 = {
     "firstName": "Naruto",
     "lastName": "Uzumaki",
     "address": "Konoha, 142 apt.",
@@ -9,4 +9,16 @@ order_data = {
     "deliveryDate": "2020-06-06",
     "comment": "Saske, come back to Konoha",
     "color": ["BLACK"]
+}
+
+order_data_2 = {
+    "firstName": "Sasuke",
+    "lastName": "Uchiha",
+    "address": "Oto, 123 apt.",
+    "metroStation": "5",
+    "phone": "+7 800 355 35 36",
+    "rentTime": 3,
+    "deliveryDate": "2020-06-07",
+    "comment": "Naruto, let's fight",
+    "color": ["RED", "BLUE"]
 }
