7ed8600f2461cc4b03d8617e1f0bad86543111df
ElvinGP <bricks7@icloud.com>
2025-03-15 21:10:16 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,14 +1,15 @@
 # sender_stand_request.py
 import requests
 from configuration import BASE_URL, CREATE_ORDER_PATH, GET_ORDER_BY_TRACK_PATH
-from data import order_data
 
-def create_order():
+def create_order(body):
+
     url = BASE_URL + CREATE_ORDER_PATH
-    response = requests.post(url, json=order_data)
+    response = requests.post(url, json=body)
     return response
 
 def get_order_by_track(track_number):
+
     url = BASE_URL + GET_ORDER_BY_TRACK_PATH
     params = {'t': track_number}
     response = requests.get(url, params=params)
