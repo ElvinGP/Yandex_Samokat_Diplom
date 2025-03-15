1a0e87d790b637d8217bedaa9d1bf836fef0e613
ElvinGP <bricks7@icloud.com>
2025-03-15 21:38:18 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -3,13 +3,21 @@ import requests
 from configuration import BASE_URL, CREATE_ORDER_PATH, GET_ORDER_BY_TRACK_PATH
 
 def create_order(body):
-
+    """
+    Создание заказа.
+    :param body: Тело запроса для создания заказа.
+    :return: Ответ от API.
+    """
     url = BASE_URL + CREATE_ORDER_PATH
     response = requests.post(url, json=body)
     return response
 
 def get_order_by_track(track_number):
-
+    """
+    Получение заказа по треку.
+    :param track_number: Номер трека заказа.
+    :return: Ответ от API.
+    """
     url = BASE_URL + GET_ORDER_BY_TRACK_PATH
     params = {'t': track_number}
     response = requests.get(url, params=params)
