4f342d53394fa4cb91e07f8a75c4d19ab6e56eb4
ElvinGP <bricks7@icloud.com>
2025-03-15 21:39:12 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
 # configuration.py
-BASE_URL = "https://a7ce0df0-79c3-44c0-b363-99a8d4a93198.serverhub.praktikum-services.ru/api/v1"
+BASE_URL = "https://de9a9c26-5b1c-4b23-b4f9-5705d3238c30.serverhub.praktikum-services.ru/api/v1"
 CREATE_ORDER_PATH = "/orders"
 GET_ORDER_BY_TRACK_PATH = "/orders/track"
